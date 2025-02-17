9900ccacf277159193696317e125d0a62e5e9741
wuyhyh <wuyhyh@gmail.com>
2025-02-17 13:04:30 +0800

    使用动态数组来实现可以指定容量大小的栈

---
+++ C语言程序设计/Stack_example_of_ADT/stack_v5/stackADT.c
@@ -0,0 +1,76 @@
+/******************************************************************************
+ *
+ * Name: stackADT.c - 用定长数组实现栈抽象数据类型
+ * Created on 2025/02/17
+ * Copyright (C) 2022 - 2025, wyh.
+ *
+ *****************************************************************************/
+
+#include "stackADT.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack_type {
+	Item *contents;
+	int top;
+	int size;
+};
+
+static void terminate(const char *message)
+{
+	printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+Stack create(int size)
+{
+	Stack s = malloc(sizeof(struct stack_type));
+	if (s == NULL) {
+		terminate("Error in create: stack could not be created.");
+	}
+	s->contents = malloc(size * sizeof(Item));
+	if (s->contents == NULL) {
+		free(s);
+		terminate("Error in create: stack could not be created.");
+	}
+	s->top = 0;
+	s->size = size;
+	return s;
+}
+
+void destroy(Stack s)
+{
+	free(s->contents);
+	free(s);
+}
+
+void make_empty(Stack s)
+{
+	s->top = 0;
+}
+
+bool is_empty(Stack s)
+{
+	return s->top == 0;
+}
+
+bool is_full(Stack s)
+{
+	return s->top == s->size;
+}
+
+void push(Stack s, int i)
+{
+	if (is_full(s)) {
+		terminate("Error in push: stack is full.");
+	}
+	s->contents[s->top++] = i;
+}
+
+int pop(Stack s)
+{
+	if (is_empty(s)) {
+		terminate("Error in pop: stack is empty.");
+	}
+	return s->contents[--s->top];
+}
