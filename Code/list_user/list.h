46891593aab0c836e9f4a735540ecc6a4fc0429a
wuyhyh <wuyhyh@gmail.com>
2025-03-12 20:41:28 +0800

    add a linux double link list implementation in user space

--- Code/list_user/list.h
+++ Code/list_user/list.h
@@ -9,4 +9,77 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list_head {
+	struct list_head *next;
+	struct list_head *prev;
+};
+
+#define LIST_NODE_INIT(name) { &(name), &(name) }
+
+#define LIST_HEAD(name) struct list_head name = LIST_NODE_INIT(name)
+
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+	list->next = list;
+	list->prev = list;
+}
+
+static inline void __list_add(struct list_head *new, struct list_head *prev,
+			      struct list_head *next)
+{
+	prev->next = new;
+	new->prev = prev;
+	new->next = next;
+	next->prev = new;
+}
+
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head, head->next);
+}
+
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head->prev, head);
+}
+
+static inline void __list_del(struct list_head *prev, struct list_head *next)
+{
+	next->prev = prev;
+	prev->next = next;
+}
+
+static inline void list_del(struct list_head *entry)
+{
+	__list_del(entry->prev, entry->next);
+	entry->next = entry;
+	entry->prev = entry;
+}
+
+static inline int list_empty(const struct list_head *head)
+{
+	return head->next == head;
+}
+
+#define list_for_each_safe(pos, n, head)                       \
+	for (pos = (head)->next, n = pos->next; pos != (head); \
+	     pos = n, n = pos->next)
+
+#define list_for_each(pos, head) \
+	for (pos = (head)->next; pos != (head); pos = pos->next)
+
+/*
+ * 获取包含该链表节点的结构体
+ */
+#define container_of(ptr, type, member) \
+	((type *)((char *)(ptr) - (unsigned long)(&((type *)0)->member)))
+
+#define list_for_each_entry(pos, head, member)                       \
+	for (pos = container_of((head)->next, typeof(*pos), member); \
+	     &pos->member != (head);                                 \
+	     pos = container_of(pos->member.next, typeof(*pos), member))
+
 #endif //LIST_H
--- Code/list_user/test_list.c
+++ Code/list_user/test_list.c
@@ -5,3 +5,46 @@
  * Copyright (C) 2022 - 2025, wyh.
  *
  *****************************************************************************/
+
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct my_node {
+	int data;
+	struct list_head list;
+};
+
+int main(void)
+{
+	LIST_HEAD(my_list); // 定义链表头
+	struct my_node *entry;
+
+	// 添加100个节点
+	for (int i = 0; i < 100; i++) {
+		struct my_node *node =
+			(struct my_node *)malloc(sizeof(struct my_node));
+		if (!node) {
+			perror("malloc failed");
+			exit(EXIT_FAILURE);
+		}
+		node->data = i;
+		list_add_tail(&node->list, &my_list);
+		printf("added data %d at address %p\n", i, node);
+	}
+
+	// 遍历链表并输出
+	list_for_each_entry(entry, &my_list, list) {
+		printf("Data: %d at address %p, next: %p, prev: %p\n",
+		       entry->data, entry, entry->list.next, entry->list.prev);
+	}
+
+	// 释放链表节点
+	struct list_head *pos, *n;
+	list_for_each_safe(pos, n, &my_list) {
+		entry = container_of(pos, struct my_node, list);
+		free(entry);
+	}
+
+	return 0;
+}
