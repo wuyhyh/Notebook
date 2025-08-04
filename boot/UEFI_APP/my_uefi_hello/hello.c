e56f108f21c973e4fd26ecd092d572b0b21b2fd2
wuyhyh <wuyhyh@gmail.com>
2025-08-04 23:10:25 +0800

    stall

--- boot/UEFI_APP/my_uefi_hello/hello.c
+++ boot/UEFI_APP/my_uefi_hello/hello.c
@@ -1,15 +1,14 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
-{
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	InitializeLib(ImageHandle, SystemTable);
 
 	int count = 0;
 	while (1) {
 		Print(L"[UEFI] Hello! Count = %d\n", count++);
-		// 延时 1 秒（单位为微秒）
-		udelay(1000000);
+		// 延时 1 秒 = 1,000,000 微秒
+		SystemTable->BootServices->Stall(1000000);
 	}
 
 	return EFI_SUCCESS;
